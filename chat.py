b0c9fcd7e203c05f06d861e77584e990cfeeeb11
Stefan Auditor <stefan.auditor@erdfisch.de>
2017-07-06 08:52:24 +0200

    Initial import

---
+++ chat.py
@@ -0,0 +1,17 @@
+# A simple internet-chat application
+
+import network
+import sys
+
+def heard(phrase):
+  print("them:" + phrase)
+
+if (len(sys.argv) >= 2):
+  network.call(sys.argv[1], whenHearCall=heard)
+else:  
+  network.wait(whenHearCall=heard)
+
+while network.isConnected():
+  phrase = input()
+  print("me:" + phrase)
+  network.say(phrase)
